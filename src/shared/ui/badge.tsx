5a37b700c0f81bda49c8f84a515170210b082881
st1vv <s.ivanov@scroogefrog.com>
2026-04-14 11:34:45 +0300

    fixed badge

--- src/shared/ui/badge.tsx
+++ src/shared/ui/badge.tsx
@@ -8,7 +8,7 @@ interface BadgeProps {
 export const Badge = ({ children, className = "" }: BadgeProps) => {
   return (
     <div
-      className={`inline-flex rounded-full border border-[#7132f5]/30 bg-[#7132f5]/10 text-sm font-medium text-[#a884ff] ${className}`}
+      className={`text-nowrap inline-flex rounded-full border border-[#7132f5]/30 bg-[#7132f5]/10 text-sm font-medium text-[#a884ff] ${className}`}
     >
       {children}
     </div>
